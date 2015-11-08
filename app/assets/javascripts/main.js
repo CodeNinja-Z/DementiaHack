93089e101cc6745569c264f1048d5abe7baf9e51
Max Hartshorn <max.hartshorn@gmail.com>
2015-11-08 11:21:27 -0500

    dynamic schedule page setup

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,4 +16,70 @@ $(document).ready(function(){
     	$(this).parent().remove();
     });
 
+
+    /* populate schedule and bind events */
+    if ( $('.bubble').length > 0 ) {
+    	var d = new Date();
+    	var day = d.getDay();
+    	var time = getMedTime(d);
+    	var ready = readyForMeds(d);
+    	day = 5;
+
+		for (var i = 0; i < day; i++ ) {
+    		var row = $('.bubble tr').eq(i + 1);
+    		$(row).find('td:not(".label")').each(function() {
+    			$(this).addClass('correct');
+    			console.log($(this))
+    		});
+    	}
+
+	   	// this days events
+	   	if ( time > 0 ) {
+	   		$('.bubble tr').eq(day + 1).find('td').eq(1).addClass('correct');
+	   	} 
+	   	if ( time > 1 ) {
+	   		$('.bubble tr').eq(day + 1).find('td').eq(2).addClass('correct');
+	   	}
+	   	if ( time > 2 ) {
+	   		$('.bubble tr').eq(day + 1).find('td').eq(3).addClass('correct');
+	   	}
+	   	if ( time > 3 ) {
+	   		$('.bubble tr').eq(day + 1).find('td').eq(4).addClass('correct');
+	   	}
+
+	   	// next event
+	   	if ( time < 4 ) {
+	   		$('.bubble tr').eq(day + 1).find('td').eq( time + 1 ).addClass('next');
+	   	}
+    }
+
+    function getMedTime(d) {
+    	var h = d.getHours();
+    	if (h < 9) {
+    		return 0;
+    	} else if (h < 13) {
+    		return 1;
+    	} else if (h < 18) {
+    		return 2;
+    	} else if (h < 20) {
+    		return 3;
+    	} else {
+    		return 4;
+    	}
+    }
+
+    function readyForMeds(d) {
+		var h = d.getHours();
+    	if (h >= 7 && h < 9) {
+    		return true;
+    	} else if (h >= 11 && h < 13) {
+    		return true;
+    	} else if (h >= 16 && h < 18) {
+    		return true;
+    	} else if (h >= 20 && h < 22) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
 });
