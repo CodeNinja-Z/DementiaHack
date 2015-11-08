ad3305578f667ec1c065513715458f1f84fc7421
Max Hartshorn <max.hartshorn@gmail.com>
2015-11-07 21:11:32 -0500

    front end schedule grid layout added

---
+++ app/controllers/schedule_controller.rb
@@ -0,0 +1,4 @@
+class ScheduleController < ApplicationController
+  def monitor
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'schedule/monitor'
+
   get 'medications/index'
 
   get 'medications/new'
